7f9cfe25e5b0bc6de6e86cc817bc78026605bf7e
OgheneDev <emmanueloghene72@gmail.com>
2025-06-20 07:50:27 +0100

    smh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import StatsGrid from "@/components/dashboard/StatsGrid";
 
 export default function Home() {
   return (
-    <div className="mt-10">
+    <div className="mt-10 mb-10 md:mb-0">
       <div>
         <ChartsGrid />
         <StatsGrid />
